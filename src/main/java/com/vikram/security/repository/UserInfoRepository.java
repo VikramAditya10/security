99c18cab5cb89ee543b29c65d5d3f7bc8449b42f
Vikram Aditya <vikkiranger@gmail.com>
2024-02-22 01:31:07 +0530

    Initial commit

---
+++ src/main/java/com/vikram/security/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.vikram.security.repository;
+
+import com.vikram.security.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
---
+++ src/test/java/com/vikram/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
